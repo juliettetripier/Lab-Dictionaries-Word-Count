37a8cbfee96dd572cf0a85fdfa74af1ace9796e0
Juliette <juli.tripier@gmail.com>
2023-09-05 15:53:06 -0400

    Finished main lab

--- wordcount.py
+++ wordcount.py
@@ -2,3 +2,28 @@
 
 
 # put your code here.
+def load_data(filename):
+    file = open(filename)
+    all_lines = []
+
+    for line in file:  
+        each_line = line.rstrip(".?,;:!_-\n").split(" ")
+
+        for word in each_line:
+            all_lines.append(word)
+
+    return all_lines
+
+all_lines = load_data("twain.txt")
+
+#print(load_data("test.txt"))
+
+def count_words():
+    word_counts = {}
+    for word in all_lines:
+        word_counts[word] = word_counts.get(word, 0) + 1
+    return word_counts
+
+print(count_words())
+
+# """Count letters in phrase.
